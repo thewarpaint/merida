621182c953a1a6af7fd2e883b4f1d01cef51390d
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-05-17 17:10:15 -0500

    Add company RFC test, disable debug output

--- rfc/tests.ts
+++ rfc/tests.ts
@@ -1,4 +1,7 @@
 import {checksum} from './checksum.ts';
 
+// Company RFCs are 12 characters long
+console.assert(checksum('SPM1410037E8') === '8');
+
 // Person RFCs are 13 characters long
-console.assert(checksum('GODE561231GR8', true) === '8');
+console.assert(checksum('GODE561231GR8') === '8');

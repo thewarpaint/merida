547d55b53790b6d01a70f999a347a783545dc48f
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-05-17 00:19:43 -0500

    Add complete RFC checksum algorithm

--- rfc/checksum.ts
+++ rfc/checksum.ts
@@ -1,6 +1,31 @@
-// Assumes rfc is already in compact format
-export function checksum(rfc: string): number {
-  return 0;
+// Used to map characters to values used in checksum calculation
+// 0 is 0, 1 is 1, ..., space is 37, Ñ is 38
+export const RFC_ALPHABET = '0123456789ABCDEFGHIJKLMN&OPQRSTUVWXYZ Ñ';
+
+// Assumes rfc is already in compact format and all uppercase
+export function checksum(rfc: string): string {
+  // Drop the checksum character (last one)
+  const rfcWithoutChecksum = rfc.substring(0, rfc.length - 1);
+
+  // Add spaces at the beginning of the string to make it 12-chars long
+  // Only necessary for corporations
+  const paddedRfc = rfcWithoutChecksum.padStart(12);
+
+  const sum = paddedRfc
+    .split('')
+    .reduce((sumAcc: number, char: string, i: number) => {
+      return sumAcc + (
+        // The position of the current character in the RFC_ALPHABET lookup table
+        RFC_ALPHABET.indexOf(char) *
+        // The position of the current character in the string, from right to left, plus one
+        (13 - i)
+      );
+    }, 0);
+
+  // We calculate sum modulo 11, then subtract that from 11
+  const checksumPosition = 11 - (sum % 11);
+
+  return RFC_ALPHABET[checksumPosition];
 }
 
-console.log(checksum('abcd010101ab1'));
+console.assert(checksum('GODE561231GR8') === '8');

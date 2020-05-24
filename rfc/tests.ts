317572ff2ea80f626a1837e24368f12f6521c39d
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-05-24 12:58:20 -0500

    Add assertChecksum method to tests

--- rfc/tests.ts
+++ rfc/tests.ts
@@ -1,7 +1,14 @@
 import {checksum} from './checksum.ts';
 
 // Company RFCs are 12 characters long
-console.assert(checksum('SPM1410037E8') === '8');
+assertChecksum('SPM1410037E8', '8');
 
 // Person RFCs are 13 characters long
-console.assert(checksum('GODE561231GR8') === '8');
+assertChecksum('GODE561231GR8', '8');
+
+function assertChecksum(rfc: string, expectedChecksum: string) {
+  const actualChecksum = checksum(rfc);
+
+  console.assert(actualChecksum === expectedChecksum,
+    `Checksum for ${rfc} was expected to be "${expectedChecksum}", but "${actualChecksum}" was found`);
+}
